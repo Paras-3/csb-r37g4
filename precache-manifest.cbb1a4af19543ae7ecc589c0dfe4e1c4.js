bd173972d2b1f4d97b87acfe732e92b1cbad0b9e
Code Sandbox <hello@codesandbox.io>
2022-01-19 15:20:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cbb1a4af19543ae7ecc589c0dfe4e1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4c77c1ee9bf2e3cf0d914b914345e6",
+    "url": "https://paras-3.github.io/csb-r37g4/index.html"
+  },
+  {
+    "revision": "90acfb9a29ec2bb6b7ff",
+    "url": "https://paras-3.github.io/csb-r37g4/static/css/main.6cfff724.chunk.css"
+  },
+  {
+    "revision": "07ef673aad38f3f2a41b",
+    "url": "https://paras-3.github.io/csb-r37g4/static/js/2.e8d6b5ed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://paras-3.github.io/csb-r37g4/static/js/2.e8d6b5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90acfb9a29ec2bb6b7ff",
+    "url": "https://paras-3.github.io/csb-r37g4/static/js/main.bb64d654.chunk.js"
+  },
+  {
+    "revision": "8c1ff695135f71ec5e90",
+    "url": "https://paras-3.github.io/csb-r37g4/static/js/runtime-main.37afff11.js"
+  }
+]);
